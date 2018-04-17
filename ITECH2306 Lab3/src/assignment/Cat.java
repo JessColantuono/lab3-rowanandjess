ad55093ab572d8b6776a913f1965800441a9284b
JessicaColantuono <37563322+JessicaColantuono@users.noreply.github.com>
2018-04-17 20:10:52 +1000

    Update Cat.java

--- ITECH2306 Lab3/src/assignment/Cat.java
+++ ITECH2306 Lab3/src/assignment/Cat.java
@@ -1,25 +1,22 @@
 package assignment;
 
 /**
- * @author 30333241
- * @version 1.0
- * @created 10-Apr-2018 12:38:50 PM
+ * @author 30333241 - Rowan Farrell & 30126235- Jessica Colantuono
  */
 public class Cat extends Animal {
 
+	public double regFee = 20;
 	public int microchipID;
-	public int catRegFee= 10;
+	
 
+	
 	public Cat(String _name, String _breed,String _age, String _regDueDate) {
 	this.name = _name;
 	this.breed = _breed;
 	this.age = _age;
-	this.regDueDate = _regDueDate;
+	this.currentRegDueDate = _regDueDate;
 	}
 
-	
-	public int getCatRegFee() {
-		return catRegFee;}
 	
 	public void finalize() throws Throwable {
 		super.finalize();
