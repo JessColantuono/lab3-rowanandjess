79cbeab935e87a8470d016eb687294e1e4333c0e
JessicaColantuono <37563322+JessicaColantuono@users.noreply.github.com>
2018-04-17 20:11:49 +1000

    Update Dog.java

--- ITECH2306 Lab3/src/assignment/Dog.java
+++ ITECH2306 Lab3/src/assignment/Dog.java
@@ -1,60 +1,64 @@
 package assignment;
 
 /**
- * @author 30333241
- * @version 1.0
- * @created 10-Apr-2018 12:38:51 PM
+ * @author 30333241 - Rowan Farrell & 30126235- Jessica Colantuono
  */
 public class Dog extends Animal {
 
-	/**
-	 * @return the desexed
-	 */
-	private boolean isDesexed() {
-		return desexed;
-	}
-
-	/**
-	 * @param desexed the desexed to set
-	 */
-	private void setDesexed(boolean desexed) {
-		this.desexed = desexed;
-	}
-
-	/**
-	 * @return the microchipID
-	 */
-	private int getMicrochipID() {
-		return microchipID;
-	}
-
-	/**
-	 * @param microchipID the microchipID to set
-	 */
-	private void setMicrochipID(int microchipID) {
-		this.microchipID = microchipID;
-	}
-
-	/**
-	 * @return the vaccination
-	 */
-	private boolean isVaccination() {
-		return vaccination;
-	}
-
-	/**
-	 * @param vaccination the vaccination to set
-	 */
-	private void setVaccination(boolean vaccination) {
-		this.vaccination = vaccination;
-	}
-
+	public double regFee= 25;
 	public boolean desexed;
 	public int microchipID;
 	public boolean vaccination;
 
+	@Override
+	public double getFee() {
+		if (desexed) {
+			return regFee;
+		} else {
+			return regFee * 1.3;
+		}
+		
+	}
+	
+	private boolean isDesexed() {
+		return desexed;
+	}
+
+	private void setDesexed(boolean desexed) {
+		this.desexed = desexed;
+	}
+
+	private int getMicrochipID() {
+		return microchipID;
+	}
+
+	private void setMicrochipID(int microchipID) {
+		this.microchipID = microchipID;
+	}
+
+	private boolean isVaccination() {
+		return vaccination;
+	}
+	
+	private void setVaccination(boolean vaccination) {
+		this.vaccination = vaccination;
+	}
+
 	public Dog(){
 
+	}
+	
+	private static boolean isRestrictedBreed(String breed) {
+
+		switch(breed.toLowerCase()){ 
+		case "american pit bull terrier": case "pit bull terrier": 
+		case "dogo argentino": case "fila brasileiro": case "japanese tosa": 
+		case "perro de presa canario": case "presa canario":
+			return true;
+
+		default:
+			return false;
+		}
 	}
 
 	public void finalize() throws Throwable {
